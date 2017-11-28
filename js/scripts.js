c48278086b5b3be05edce75c6662c4dca5de7d96
Danyel Murray and Zach Evans <zach@>
2017-11-27 16:07:32 -0800

    Add 2nd function to reverse sentence order.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 var sentence = prompt("Please enter a sentence");
-alert(sentence);
 
 var endcaps = function(sentence){
   return sentence.charAt(0).toUpperCase() + sentence.charAt(sentence.length-1).toUpperCase()
 };
 
 alert(endcaps(sentence));
+
+var reversed = function(sentence){
+  return endcaps(sentence).charAt(1) + endcaps(sentence).charAt(0);
+  //var splitReverse = endcaps(sentence).split("");
+  //var reverseReverse = splitReverse.reverse();
+  //var joinReverse = reverseReverse.join("");
+  //return joinReverse;
+};
+
+alert(reversed(sentence));
