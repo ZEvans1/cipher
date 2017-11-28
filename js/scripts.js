5aaca256484d1d15acc972704f69b3f9cca61b45
Danyel Murray and Zach Evans <zach@>
2017-11-27 16:36:33 -0800

    add reverse section to original sentence

--- js/scripts.js
+++ js/scripts.js
@@ -6,12 +6,23 @@ var endcaps = function(sentence){
 
 alert(endcaps(sentence));
 
-var reversed = function(sentence){
-  return endcaps(sentence).charAt(1) + endcaps(sentence).charAt(0);
+var oldandrev = function(sentence){
+  return sentence + endcaps(sentence).charAt(1) + endcaps(sentence).charAt(0);
   //var splitReverse = endcaps(sentence).split("");
   //var reverseReverse = splitReverse.reverse();
   //var joinReverse = reverseReverse.join("");
   //return joinReverse;
+
 };
 
-alert(reversed(sentence));
+alert(oldandrev(sentence));
+
+var count = function(sentence){
+  alert(sentence.charAt(sentence.length/2));
+  return sentence.charAt(sentence.length/2) + oldandrev(sentence);
+};
+
+alert(count(sentence));
+
+
+}
